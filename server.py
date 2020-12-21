50d52c75d846b3254b37963455bc65c069621ad2
sharmita3 <49931235+sharmita3@users.noreply.github.com>
2020-12-21 12:53:13 -0500

    Update server.py

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        msg = 'Hello there!! You requested %s' % (self.path)
+        msg = 'Hello there! You requested %s' % (self.path)
         self.wfile.write(msg.encode())
 
 port = int(os.getenv('PORT', 80))
